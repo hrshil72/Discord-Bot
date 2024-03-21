813eaa5abc3209a2e9ecd1e3bcd0dc9d6e94ff4b
[Harshil Karir] <harshilkarir72@gmail.com>
2024-03-21 12:24:44 +0530

    basic setup

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import "dotenv/config";
+
+import { Client, GatewayIntentBits } from "discord.js";
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", (e) => {
+  console.log(`${e.user.username} is online`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "Hello") {
+    message.reply("Hello");
+  }
+});
+
+client.login(process.env.TOKEN);
